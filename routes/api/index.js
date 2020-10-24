8d9d2667b82f227976d601e5fa7186f1fddc9b34
Jennifer Schlierman <jd9913@gmail.com>
2020-10-24 16:44:31 -0700

    updating routes to get the server working

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,8 +3,6 @@ const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
-// router.use('/categories', category-routes);
-// router.use('/products', product-routes);
-// router.use('/tags', tag-routes);
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,9 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Wrong Route!</h1>").end();
 });
 
-router.use('/categories', category-routes);
-router.use('/products', product-routes);
-router.use('/tags', tag-routes);
+
 
 module.exports = router;
